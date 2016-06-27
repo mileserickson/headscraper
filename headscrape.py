8b5d57d4941a6b49afbdb23175332e51edbfb591
Miles Erickson <miles.erickson@gmail.com>
2016-06-27 13:43:58 -0700

    Refactor

--- headscrape.py
+++ headscrape.py
@@ -15,18 +15,13 @@ class HeadScraper(object):
         self.cols = cols
         self.mbox = mbox(mbox_name)
 
-    @property
-    def headers(self):
-        """Yield each message header as a dictionary."""
-        for msg in self.mbox:
-            yield {col: msg[col] for col in self.cols}
-
     def write_csv(self, csv_name):
         """Output message headers to a CSV file."""
         with open(csv_name, 'w') as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=self.cols)
             writer.writeheader()
-            for header in self.headers:
+            for msg in self.mbox:
+                header = {col: msg[col] for col in self.cols}
                 writer.writerow(header)
 
 
@@ -35,11 +30,10 @@ if __name__ == "__main__":
         print("Usage: python headscrape.py sample.mbox [sample.csv]")
     else:
         mbox_name = sys.argv[1]
-        hs = HeadScraper(mbox_name)
         if len(sys.argv) > 2:
             csv_name = sys.argv[2]
         else:
             csv_name = "{}.csv".format(mbox_name)
         print("Writing {}...".format(csv_name))
-        hs.write_csv(csv_name)
+        HeadScraper(mbox_name).write_csv(csv_name)
 
