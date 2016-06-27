9c2943445ea08374df094a6b6dee70baa498cd4b
Miles Erickson <miles.erickson@gmail.com>
2016-06-27 13:37:41 -0700

    Refactor HeadScraper

--- headscrape.py
+++ headscrape.py
@@ -7,20 +7,19 @@ import csv
 import sys
 
 
-class HeadScraper(mbox):
+class HeadScraper(object):
     """Scrape the headers of messages from an mbox file."""
 
     def __init__(self, mbox_name, cols=["To", "From", "Subject", "Date"]):
         """Create a new HeadScraper."""
         self.cols = cols
-        mbox.__init__(self, mbox_name)
+        self.mbox = mbox(mbox_name)
 
     @property
     def headers(self):
         """Yield each message header as a dictionary."""
-        for msg in self:
-            header = {col: msg[col] for col in self.cols}
-            yield header
+        for msg in self.mbox:
+            yield {col: msg[col] for col in self.cols}
 
     def write_csv(self, csv_name):
         """Output message headers to a CSV file."""
@@ -32,15 +31,15 @@ class HeadScraper(mbox):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
+    if len(sys.argv) < 2:
+        print("Usage: python headscrape.py sample.mbox [sample.csv]")
+    else:
         mbox_name = sys.argv[1]
         hs = HeadScraper(mbox_name)
         if len(sys.argv) > 2:
             csv_name = sys.argv[2]
         else:
-            csv_name = mbox_name+".csv"
+            csv_name = "{}.csv".format(mbox_name)
         print("Writing {}...".format(csv_name))
         hs.write_csv(csv_name)
 
-    else:
-        print("Usage: python headscrape.py sample.mbox [sample.csv]")
